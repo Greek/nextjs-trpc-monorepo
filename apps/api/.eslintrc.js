d8a9022ebd77940cb7bce6d0bb61e5874aecf279
Andreas <git@fsck.rip>
2025-05-13 14:25:54 -0400

    package: rename all internal packages from `@repo/` to `@stashbase/`
    Signed-off-by: Andreas <git@fsck.rip>

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -1,5 +1,5 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/server.js"],
+  extends: ["@stashbase/eslint-config/server.js"],
 };
