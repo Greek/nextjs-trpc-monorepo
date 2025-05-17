907b7a54376919097a0bea1c44e64fd7022d3d70
Andreas <git@fsck.rip>
2025-05-17 15:28:30 -0400

    api: remove logger
    Signed-off-by: Andreas <git@fsck.rip>

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,9 @@
 import { createServer } from "./server";
-import { log } from "@repo/logger";
 import { env } from "./utils/env";
 
 const port = env.PORT || 3001;
 const server = createServer();
 
 server.listen(port, () => {
-  log(`api running on ${port}`);
+  console.log(`api running on ${port}`);
 });
