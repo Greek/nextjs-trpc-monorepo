16b7ea92ff62cf0d06403e9b7887adbbe7dd387a
Andreas <git@fsck.rip>
2025-05-24 23:53:14 -0700

    api(hello-world): only export the router from the module, not the initTRPC obj
    Signed-off-by: Andreas <git@fsck.rip>

--- apps/api/src/modules/hello-world/hello-world.router.ts
+++ apps/api/src/modules/hello-world/hello-world.router.ts
@@ -3,7 +3,7 @@ import { initTRPC } from "@trpc/server";
 import { HelloWorldModule } from "./hello-world.module";
 import { protectedProcedure, publicProcedure } from "../../lib/trpc";
 
-export const t = initTRPC.create();
+const t = initTRPC.create();
 export const helloWorldRouter = t.router({
   getName: publicProcedure.input(z.string().min(2)).mutation(async (opts) => {
     return HelloWorldModule.build().getName(opts.input);
