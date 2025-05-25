6d7dd49ae08294873f070bea0c7887e383736376
Andreas <git@fsck.rip>
2025-05-25 14:21:33 -0700

    oops
    Signed-off-by: Andreas <git@fsck.rip>

--- apps/api/src/lib/constants.ts
+++ apps/api/src/lib/constants.ts
@@ -2,3 +2,4 @@ import { env } from "./env";
 
 export const ALLOWED_ORIGINS = env.CORS_ALLOWED_ORIGINS?.split(",");
 export const MIN_PASSWORD_LENGTH = /.{8,}/;
+export const MAX_PASSWORD_LENGTH = /.{128,}/;
