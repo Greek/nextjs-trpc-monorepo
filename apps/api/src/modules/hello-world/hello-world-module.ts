a6e3d1bf5a5e92d20f46957826af04ae66a761f4
Andreas <git@fsck.rip>
2025-06-19 12:36:00 -0700

    api: log out the sesh
    Signed-off-by: Andreas <git@fsck.rip>

--- apps/api/src/modules/hello-world/hello-world-module.ts
+++ apps/api/src/modules/hello-world/hello-world-module.ts
@@ -1,28 +1,24 @@
 import { auth } from '@/lib/auth';
-import { logger } from '@/lib/logger';
+import { Logger } from '@/lib/logger';
 import { inferProcedureBuilderResolverOptions } from '@trpc/server';
 import type { Request } from 'express';
-import winston from 'winston';
 import { protectedProcedure } from '../../lib/trpc';
 export class HelloWorldModule {
-  constructor(private logger: winston.Logger) {}
+  constructor() {}
 
   public static build() {
-    const _logger = logger.child({
-      meta: { module: 'hello-world' },
-    });
-    return new HelloWorldModule(_logger);
+    return new HelloWorldModule();
   }
 
   public async getName(
     opts: inferProcedureBuilderResolverOptions<typeof protectedProcedure>,
   ) {
     const req = opts.ctx.req as Request;
-
     const freshSesh = await auth.api.getSession({
       headers: Object.assign(req.headers),
     });
-    logger.info(freshSesh);
+
+    Logger.getInstance().info(freshSesh);
 
     if (!opts.ctx.session) {
       return `Hello ${opts.input}! What's up?`;
